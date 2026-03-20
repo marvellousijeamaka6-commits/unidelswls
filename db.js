d4ea7c1224e3deeb6cc241c08cde50e70594d3ec
marvellousijeamaka6-commits <marvellousijeamaka6@gmail.com>
2026-03-20 14:34:09 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,6 +2,6 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: "YOUR_NEON_DATABASE_URL",
+  connectionString: postgresql://neondb_owner:npg_e1mFW9YQlxqG@ep-sparkling-frost-a8lhvtdo-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require,
   ssl: { rejectUnauthorized: false }
 });
